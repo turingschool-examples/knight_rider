78705c28ec404da746972ba010e19eaba436b160
marcoverzero <marcoverzero@gmail.com>
2017-02-28 10:03:18 -0700

    test issue # of arguments

--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -11,4 +11,12 @@ class CharacterTest < Minitest::Test
     assert_equal "marc", marc.name
 
   end
+  def test_the_character_has_an_actor
+    kitt = Character.new("KITT", "William Daniels")
+    assert_equal "William Daniels", kitt.actor
+  end
+  def test_the_character_has_a_description
+    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am")
+    assert_equal "William Daniels", kitt.description
+  end
 end
