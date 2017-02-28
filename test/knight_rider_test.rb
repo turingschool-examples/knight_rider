1e0e92718f1a3d62841bf2983a4e481b37919031
bschwartz10 <bschwartz92@gmail.com>
2017-02-28 10:36:38 -0700

    add knight_rider class

--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -2,13 +2,42 @@ gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/knight_rider'
+require './lib/character'
 
 class KnightRiderTest < Minitest::Test
 
   def test_it_exists
+    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
     knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
 
     assert_instance_of KnightRider, knight_rider
+    assert_instance_of Character, kitt
+    assert_instance_of Character, michael_knight
+  end
+
+  def test_knight_rider_has_characters
+    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
+
+    assert_equal [kitt, michael_knight], knight_rider.characters
+  end
+
+  def test_knight_rider_has_a_creator
+    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
+
+    assert_equal "Glen Larson", knight_rider.creator
+  end
+
+  def test_knight_rider_has_a_network
+    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
+
+    assert_equal "NBC", knight_rider.network
   end
 
 end
