57dd8eeefb06eac10f2934e30fa11c71ee9c0e62
jk1dd <portajonk@gmail.com>
2017-02-28 10:04:16 -0700

    complete iteration 2

--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -8,12 +8,15 @@ class KnightRiderTest < Minitest::Test
 
   attr_reader :knight_rider,
               :kitt,
-              :michael_knight
+              :michael_knight,
+              :devon_miles
 
   def setup
     @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
     @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    @devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
     @knight_rider = KnightRider.new([@kitt, @michael_knight], "Glen Larson", "NBC")
+    # @knight_rider_2 = KnightRider.new([@kitt, @michael_knight, @devon_miles], "Glen Larson", "NBC")
   end
 
   def test_it_exits
@@ -33,5 +36,18 @@ class KnightRiderTest < Minitest::Test
 
   def test_it_can_return_network
     assert_equal "NBC", knight_rider.network
+  end
+
+  def test_returns_total_payroll
+    knight_rider_2 = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+
+    assert_equal 1001000000, knight_rider_2.total_payroll
+  end
+
+  def test_returns_highest_grossing_actor_object
+    knight_rider_2 = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+
+    assert_instance_of Character, knight_rider_2.highest_grossing_actor
+    assert_equal "Michael Knight", knight_rider_2.highest_grossing_actor.name
   end
 end
