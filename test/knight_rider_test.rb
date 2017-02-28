c3b7367bbf6d422eb7e73411474c6e22b01fba0e
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-02-28 09:47:33 -0700

    updated existing iteration 1 tests to accommodate new character devon miles

--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -13,21 +13,23 @@ class KnightRiderTest < Minitest::Test
     @michael_knight = Character.new("Michael Knight",
     "David Hasselhoff", "A special type of hero",
     1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
-    @knight_rider = KnightRider.new([@kitt, @michael_knight], "Glen Larson", "NBC")
+    @devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
+    @knight_rider = KnightRider.new([@kitt, @michael_knight, @devon_miles], "Glen Larson", "NBC")
   end
 
   def test_they_exist
     assert_instance_of Character, @kitt
     assert_instance_of Character, @michael_knight
+    assert_instance_of Character, @devon_miles
     assert_instance_of KnightRider, @knight_rider
   end
 
   def test_it_returns_characters
     assert_instance_of Array, @knight_rider.characters
-    assert_equal 2, @knight_rider.characters.count
+    assert_equal 3, @knight_rider.characters.count
     assert_instance_of Character, @knight_rider.characters.first
     assert_equal @kitt, @knight_rider.characters.first
-    assert_equal @michael_knight, @knight_rider.characters.last
+    assert_equal @devon_miles, @knight_rider.characters.last
   end
 
   def test_it_returns_creator
