91a8e8cbd8b1f5a991e0a5b5317cfec384c94bee
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-02-28 11:45:32 -0700

    got parts of iteration3 going

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -1,3 +1,7 @@
+require 'webrick'
+require './lib/character'
+require 'launchy'
+
 class KnightRider
   attr_reader :creator,
               :network,
@@ -27,35 +31,22 @@ class KnightRider
     @hash_elhoff.max_by { |k,v| v }[0]
   end
 
+  def make_html
+    #convert @characters objects into printable html format
+  end
+
+  def print_to_page
+    make_html
+    writer = File.open('./lib/text.html', "w")
+    writer.write(@characters_html)
+    Launchy.open("./lib/text.html")
+  end
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
+if __FILE__ == $0
+  @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+  @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+  @devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
+  knight = KnightRider.new([@kitt, @michael_knight, @devon_miles], "Glen Larson", "NBC")
+  knight.print_to_page
+end
