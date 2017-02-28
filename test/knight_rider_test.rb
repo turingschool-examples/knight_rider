d6c1863a74221b1accd4532dfc417346648576ef
Sam Landfried <samlandfried@gmail.com>
2017-02-28 09:44:24 -0700

    Write existence test

---
+++ test/knight_rider_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/emoji'
+require './lib/knight_rider'
+
+class KnightRiderTest < Minitest::Spec
+
+  attr_reader :knight_rider
+
+  before do
+    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    @knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
+  end
+
+  describe "when it starts" do
+    it "should be a KnightRider" do
+      knight_rider.must_be_instance_of(KnightRider)
+    end
+  end
+end
