726459bb7334a5106e249ca65644f1671f0f66b4
NatBee <nataliesbarron@gmail.com>
2017-02-28 09:53:53 -0700

    completed iteration 1 kitt character tdd. All tests pass.

---
+++ test/knight_rider_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/emoji'
+require 'pry'
+require './lib/knight_rider'
+require './lib/character'
+
+class KnightRiderTest < Minitest::Test
+attr_reader :kitt
+
+  def setup
+    @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+
+  end
+
+  def test_it_exists
+    assert_instance_of Character, kitt
+  end
+
+  def test_it_returns_name
+    assert_equal "KITT", kitt.name
+  end
+
+  def test_it_returns_actor
+    assert_equal "William Daniels", kitt.actor
+  end
+
+  def test_it_returns_description
+    assert_equal "Pontiac Trans-Am", kitt.description
+  end
+
+  def test_it_returns_birth_year
+    assert_equal 1982, kitt.birth_year
+  end
+
+  def test_it_returns_a_salary
+    assert_equal 0, kitt.salary
+  end
+
+  def test_it_returns_headshot_url
+    assert_equal "https://goo.gl/lSFYQo", kitt.headshot_url
+  end
+
+end
