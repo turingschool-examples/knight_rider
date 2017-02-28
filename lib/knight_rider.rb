1571c232b97d5ae33c13b6264bbdc0f90f3a03ab
glassjoseph <glassjoseph@gmail.com>
2017-02-28 09:43:23 -0700

    added character and character test

---
+++ lib/knight_rider.rb
@@ -0,0 +1,14 @@
+kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+#=> <Character:...>
+kitt.name
+#=> "KITT"
+kitt.actor
+#=> "William Daniels"
+kitt.description
+#=> "Pontiac Trans-Am"
+kitt.birth_year
+#=> 1982
+kitt.salary
+#=> 0
+kitt.headshot_url
+#=> "https://goo.gl/lSFYQo"
---
+++ test/knight_rider_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/knight_rider'
+
+
+
+class KnightRiderTest < Minitest::Test
+  def test_it_exists
+    # kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    # assert_instance_of Character, kitt
+
+    
+  end
+
+end
