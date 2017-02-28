c4e37d558dd14b4edcad53e74fce88ce051c86ae
Beth Knight <bethknight1234@gmail.com>
2017-02-28 10:13:08 -0700

    iteration 2

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -1,11 +1,25 @@
 class KnightRider
 
-  attr_reader :characters, :creator, :network
+  attr_reader :characters, :creator, :network, :total_payroll, :highest_grossing_actor
 
   def initialize(characters, creator, network )
     @characters = characters
     @creator = creator
     @network = network
+  end
+
+  def total_payroll
+
+    payroll = 0
+
+    characters.each do |character|
+      payroll += character.salary.gsub(/\,/,"").to_i
+    end
+    payroll
+  end
+
+  def highest_grossing_actor
+    characters.sort_by { |character| character.salary.gsub(/\,/,"").to_i }.reverse.first 
   end
 
 end
--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -5,11 +5,12 @@ require './lib/character'
 
 class KnightRiderTest < Minitest::Test
 
-  attr_reader :kitt, :michael_knight, :knight_rider
+  attr_reader :kitt, :michael_knight, :devon_miles, :knight_rider
 
   def setup
     @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
     @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    @devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
     @knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
   end
 
@@ -23,6 +24,18 @@ class KnightRiderTest < Minitest::Test
 
   def test_it_finds_a_network
     assert_equal "NBC", knight_rider.network
+  end
+
+  def test_it_finds_total_payroll
+    knight_rider = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+
+    assert_equal 1001000000, knight_rider.total_payroll
+  end
+
+  def test_it_finds_the_highest_grossing_actor
+    knight_rider = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+
+    assert_equal michael_knight, knight_rider.highest_grossing_actor
   end
 
 end
