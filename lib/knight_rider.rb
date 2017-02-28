ebac4b3a9039ad0f9818c8b57d23ba597a0b54ac
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-02-28 10:04:17 -0700

    knight rider test and file

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -1,7 +1,14 @@
+require './lib/character'
+
 class KnightRider
+  attr_reader :creator,
+              :network,
+              :characters
 
-  def initialize
-
+  def initialize(character_array, creator, network)
+    @creator = creator
+    @network = network
+    @characters =  character_array
   end
 
 end
--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -1,22 +1,26 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/knight_rider'
+require './lib/character'
 
-class KnightRiderTest
+class KnightRiderTest < Minitest::Test
 
-  def
+  def setup
+    @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    @knight_rider = KnightRider.new([@kitt, @michael_knight], "Glen Larson", "NBC")
+  end
 
-  def test_
+  def test_has_characters
+    assert_equal [@kitt, @michael_knight], @knight_rider.characters
+  end
 
+  def test_has_creator
+    assert_equal "Glen Larson", @knight_rider.creator
+  end
+
+  def test_has_network
+    assert_equal "NBC", @knight_rider.network
   end
 
 end
-
-
-knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
-knight_rider.characters
-#=> [<Character:...>, <Character:...>]
-knight_rider.creator
-#=> "Glen Larson"
-knight_rider.network
-#=> "NBC"
