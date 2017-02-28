ed283c39d57a1b22ae2f7779e2027631f7f6e84a
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-28 10:27:06 -0700

    page generator reads ERB template

--- lib/page_generator.rb
+++ lib/page_generator.rb
@@ -2,9 +2,11 @@ require 'erb'
 require './lib/knight_rider'
 
 class PageGenerator
+  attr_reader :data, :template
 
   def initialize(data)
     @data = data
+    @template = File.read("templates/page_template.erb")
   end
 
   def load_page
--- test/page_generator_test.rb
+++ test/page_generator_test.rb
@@ -7,11 +7,16 @@ class PageGeneratorTest < Minitest::Test
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
     michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
     devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
-    @knight_rider = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+    knight_rider = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+    @pg = PageGenerator.new(knight_rider)
   end
 
-  def test_can_make_html
-    assert_instance_of PageGenerator, PageGenerator.new(@knight_rider)
+  def test_it_exists
+    assert_instance_of PageGenerator, @pg
+  end
+
+  def test_it_can_read_template
+    assert_includes @pg.template, "</html>"
   end
 
 
