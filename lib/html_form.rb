549e87a8eb91b581d50ff1d6d6039994efd8fa39
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-02-28 11:02:45 -0700

    finished tests and refactoring html page creator class

--- lib/html_form.rb
+++ lib/html_form.rb
@@ -4,19 +4,26 @@ require 'erb'
 require 'pry'
 
 class HtmlPageCreator
+  attr_reader :input_file_path, :output_file_path, :characters, :cast_page
   def initialize(input_file_path, output_file_path, knight_rider)
     @input_file_path = input_file_path
     @output_file_path = output_file_path
-    @knight_rider = knight_rider
+    @characters = knight_rider.characters
   end
-end
 
-# template_letter = File.read "./io_files/html_form.erb"
-# erb_template = ERB.new template_letter
-#
-# characters = @knight_rider.characters
-#
-# form_letter = erb_template.result(binding)
-#
-# output_letter = File.open("./io_files/html_output_file.html", "w")
-# output_letter.puts(form_letter)
+  def erb_template
+    template_cast_page = File.read @input_file_path
+    ERB.new template_cast_page
+  end
+
+  def insert_characters_into_template
+    erb_template.result(binding)
+  end
+
+  def write_html_to_output_file
+    cast_content = insert_characters_into_template
+    @cast_page = File.open(output_file_path, "w")
+    @cast_page.puts(cast_content)
+  end
+
+end
--- test/html_form_test.rb
+++ test/html_form_test.rb
@@ -9,17 +9,17 @@ require 'pry'
 class HtmlPageCreatorTest < Minitest::Test
 
   def setup
-    input_file_path = "./io_files/html_form.erb"
+    @input_file_path = "./io_files/html_form.erb"
     output_file_path = "./io_files/html_output_file.html"
     @kitt = Character.new("KITT", "William Daniels",
     "Pontiac Trans-Am", 1982,
     "0.00", "https://goo.gl/lSFYQo")
-    michael_knight = Character.new("Michael Knight",
+    @michael_knight = Character.new("Michael Knight",
     "David Hasselhoff", "A special type of hero",
     1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
-    devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
+    @devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
     knight_rider = KnightRider.new([@kitt, @michael_knight, @devon_miles], "Glen Larson", "NBC")
-    @kr_cast_page = HtmlPageCreator.new(input_file_path, output_file_path, knight_rider)
+    @kr_cast_page = HtmlPageCreator.new(@input_file_path, output_file_path, knight_rider)
   end
 
   def test_it_exists
@@ -32,6 +32,35 @@ class HtmlPageCreatorTest < Minitest::Test
     assert_equal @kitt, @kr_cast_page.characters.first
     assert_equal "./io_files/html_form.erb", @kr_cast_page.input_file_path
     assert_equal "./io_files/html_output_file.html", @kr_cast_page.output_file_path
+  end
+
+  def test_opening_erb_file
+    assert_instance_of ERB, @kr_cast_page.erb_template
+  end
+
+  def test_insert_characters_into_template
+    assert_instance_of String, @kr_cast_page.insert_characters_into_template
+  end
+
+  def test_writing_html_to_file
+    @kr_cast_page.write_html_to_output_file
+    assert_instance_of File, @kr_cast_page.cast_page
+  end
+
+  def test_it_runs_with_one_cast_member
+    output_file_path = "./io_files/html_output_file_1.html"
+    knight_rider = KnightRider.new([@kitt], "Glen Larson", "NBC")
+    kr_cast_page1 = HtmlPageCreator.new(@input_file_path, output_file_path, knight_rider)
+    kr_cast_page1.write_html_to_output_file
+    assert_instance_of File, kr_cast_page1.cast_page
+  end
+
+  def test_it_runs_with_two_cast_members
+    output_file_path = "./io_files/html_output_file_2.html"
+    knight_rider = KnightRider.new([@kitt, @michael_knight], "Glen Larson", "NBC")
+    kr_cast_page2 = HtmlPageCreator.new(@input_file_path, output_file_path, knight_rider)
+    kr_cast_page2.write_html_to_output_file
+    assert_instance_of File, kr_cast_page2.cast_page
   end
 
 end
