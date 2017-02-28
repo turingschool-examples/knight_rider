e78aa4f26a3b95db1ec81e6488046e054b87cfdd
slague <stephanielague@gmail.com>
2017-02-28 10:10:56 -0700

    Iteration 1 knight_rider and knight_rider_test

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -1,18 +1,14 @@
+require_relative 'character'
+
 class KnightRider
 
+  attr_reader :characters, :creator, :network
+
+  def initialize(characters, creator, network)
+    @characters = characters
+    @creator = creator
+    @network = network
+  end
 
 
 end
-
-
-
-
-
-
-knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
-knight_rider.characters
-#=> [<Character:...>, <Character:...>]
-knight_rider.creator
-#=> "Glen Larson"
-knight_rider.network
-#=> "NBC"
--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -1,10 +1,35 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative './lib/knight_rider'
-
+require './lib/knight_rider'
+require './lib/character'
 
 class KnightRiderTest < MiniTest::Test
 
+  attr_reader :knight_rider, :kitt, :michael_knight
 
+  def setup
+    @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+
+    @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+
+    @knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
+  end
+
+  def test_it_exists
+    assert knight_rider
+  end
+
+  def test_characters
+    assert_equal [kitt, michael_knight], knight_rider.characters
+  end
+
+  def test_creator
+    assert_equal "Glen Larson", knight_rider.creator
+
+  end
+
+  def test_network
+    assert_equal "NBC", knight_rider.network
+  end
 
 end
