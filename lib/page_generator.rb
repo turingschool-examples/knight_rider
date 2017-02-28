eefcd063fb81228a440b544b3307fedf55efe6a1
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-28 10:18:39 -0700

    framework for page generator

---
+++ lib/page_generator.rb
@@ -0,0 +1,15 @@
+require 'erb'
+require './lib/knight_rider'
+
+class PageGenerator
+
+  def initialize(data)
+    @data = data
+  end
+
+  def load_page
+
+  end
+
+
+end
---
+++ test/page_generator_test.rb
@@ -0,0 +1,18 @@
+require './test/test_helper'
+require './lib/page_generator'
+
+class PageGeneratorTest < Minitest::Test
+
+  def setup
+    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
+    @knight_rider = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+  end
+
+  def test_can_make_html
+    assert_instance_of PageGenerator, PageGenerator.new(@knight_rider)
+  end
+
+
+end
