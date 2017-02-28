4632f5fdc3adf3cf2519cd8ca21eb8fe5469fd44
slague <stephanielague@gmail.com>
2017-02-28 09:45:30 -0700

    Iteration 1, character and character_test

---
+++ lib/knight_rider.rb
@@ -0,0 +1,18 @@
+class KnightRider
+
+
+
+end
+
+
+
+
+
+
+knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
+knight_rider.characters
+#=> [<Character:...>, <Character:...>]
+knight_rider.creator
+#=> "Glen Larson"
+knight_rider.network
+#=> "NBC"
---
+++ test/knight_rider_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative './lib/knight_rider'
+
+
+class KnightRiderTest < MiniTest::Test
+
+
+
+end
