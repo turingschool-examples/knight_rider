9f44c4dfb1ca9a1d6805ff3920e0bbbfba432d1e
slague <stephanielague@gmail.com>
2017-02-28 10:39:34 -0700

    Iteration 2 complete

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -10,5 +10,13 @@ class KnightRider
     @network = network
   end
 
+  def total_payroll
+    salaries = @characters.map { |character| character.salary.delete ','}
+    salaries.map { |num| num.to_i }.reduce(:+).to_s
+  end
+
+  def highest_grossing_actor
+    @characters.max_by { |character| character.salary }
+  end
 
 end
