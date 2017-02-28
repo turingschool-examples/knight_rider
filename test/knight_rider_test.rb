4bb88f9a113a930da0b70e9b43ca036d124997a6
Sam Landfried <samlandfried@gmail.com>
2017-02-28 09:55:57 -0700

    Write .total_payroll test

--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -25,6 +25,21 @@ class KnightRiderTest < Minitest::Spec
       knight_rider.creator.must_equal("Glen Larson")
       knight_rider.network.must_equal("NBC")
     end
+  end
 
+  describe "when asked about money" do
+
+    attr_reader :knight_rider
+
+    before do
+      kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+      michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+      devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
+      @knight_rider = KnightRider.new([kitt, michael_knight, devon_miles], "Glen Larson", "NBC")
+    end
+
+    it "should know its total payroll" do
+      knight_rider.total_payroll.must_equal(1001000000)
+    end
   end
 end
