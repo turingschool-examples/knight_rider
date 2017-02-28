9d9400bed415e4e03aaa47d71a3efa02abf6998b
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-02-28 09:53:04 -0700

    finished iteration 2 all tests pass

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -7,4 +7,17 @@ class KnightRider
     @creator = creator
     @network = network
   end
+
+  def total_payroll
+    @characters.reduce(0) do |sum, character|
+      sum + character.salary
+    end
+  end
+
+  def highest_grossing_actor
+    @characters.max_by do |character|
+      character.salary
+    end
+  end
+
 end
