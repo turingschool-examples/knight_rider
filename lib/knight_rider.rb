4bd4669888c822920c826050dc24f4a59465c73e
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-02-28 10:28:25 -0700

    first logic done

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -1,5 +1,3 @@
-require './lib/character'
-
 class KnightRider
   attr_reader :creator,
               :network,
@@ -10,5 +8,18 @@ class KnightRider
     @network = network
     @characters =  character_array
   end
+
+  def convert_salary(character)
+    character.salary.gsub(/,/, '').to_f
+  end
+
+  def total_payroll
+    characters.map { |character| convert_salary(character) }.reduce(:+)
+  end
+
+  def highest_grossing_actor
+    # characters.each { |character| ch}
+  end
+
 
 end
--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -8,11 +8,12 @@ class KnightRiderTest < Minitest::Test
   def setup
     @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
     @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
-    @knight_rider = KnightRider.new([@kitt, @michael_knight], "Glen Larson", "NBC")
+    @devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
+    @knight_rider = KnightRider.new([@kitt, @michael_knight, @devon_miles], "Glen Larson", "NBC")
   end
 
   def test_has_characters
-    assert_equal [@kitt, @michael_knight], @knight_rider.characters
+    assert_equal [@kitt, @michael_knight, @devon_miles], @knight_rider.characters
   end
 
   def test_has_creator
@@ -23,4 +24,33 @@ class KnightRiderTest < Minitest::Test
     assert_equal "NBC", @knight_rider.network
   end
 
+  def test_total_payroll
+    assert_equal 1001000000, @knight_rider.total_payroll
+  end
+
+  def test_highest_paid_actor
+    assert_equal @michael_knight, @knight_rider.highest_grossing_actor
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
