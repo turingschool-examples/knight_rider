a24bd72c2bf26d24138db501e8d00168dfe9cc44
marcoverzero <marcoverzero@gmail.com>
2017-02-28 10:34:29 -0700

    more tests, one failing -- nil

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -1,11 +1,16 @@
 require 'pry'
 
 class Character
-  attr_reader :name
-    def initialize(name, actor, description)
+  attr_reader :name, :actor, :description, :birth_year, :salary, :headshot_url
+    def initialize(name, actor, decription, birth_year, salary, headshot_url)
       @name = name
       @actor = actor
       @description = description
+      @birth_year = birth_year
+      @salary = salary
+      @headshot_url = headshot_url
+
+      binding.pry
 
     end
 
--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -5,18 +5,28 @@ require './lib/knight_rider'
 
 
 class CharacterTest < Minitest::Test
+  def setup
+    @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+  end
 
   def test_character_has_a_name
-    marc = Character.new("marc")
-    assert_equal "marc", marc.name
+    assert_equal "KITT", @kitt.name
 
   end
   def test_the_character_has_an_actor
-    kitt = Character.new("KITT", "William Daniels")
-    assert_equal "William Daniels", kitt.actor
+    assert_equal "William Daniels", @kitt.actor
   end
   def test_the_character_has_a_description
-    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am")
-    assert_equal "William Daniels", kitt.description
+    assert_equal "Pontiac Trans-Am", @kitt.description
   end
+  def test_the_character_has_a_birth_year
+    assert_equal 1982, @kitt.birth_year
+  end
+  def test_the_character_has_a_salary
+    assert_equal "0.00", @kitt.salary
+  end
+  def test_the_character_has_a_headshot
+    assert_equal "https://goo.gl/lSFYQo", @kitt.headshot_url
+  end
+
 end
