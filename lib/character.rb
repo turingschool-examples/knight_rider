4c3eca4e3d1663cba55fea660f1c4d00494fa193
NessEFC <NessEFC@gmail.com>
2017-02-28 09:47:32 -0700

    all character tests passing for kitt

--- lib/character.rb
+++ lib/character.rb
@@ -8,23 +8,8 @@ class Character
     @actor = actor
     @description = description
     @birth_year = birth_year
-    @salary = salary
+    @salary = salary.to_i
     @headshot_url = headshot_url
   end
-  
-end
 
-# kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
-# #=> <Character:...>
-# kitt.name
-# #=> "KITT"
-# kitt.actor
-# #=> "William Daniels"
-# kitt.description
-# #=> "Pontiac Trans-Am"
-# kitt.birth_year
-# #=> 1982
-# kitt.salary
-# #=> 0
-# kitt.headshot_url
-# #=> "https://goo.gl/lSFYQo"
+end
--- test/character_test.rb
+++ test/character_test.rb
@@ -4,25 +4,38 @@ require 'minitest/pride'
 require './lib/character'
 
 class CharacterTest < Minitest::Test
+  attr_reader :kitt
 
-  # kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
-  # => <Character:...>
-  # kitt.name
-  # => "KITT"
-  # kitt.actor
-  # => "William Daniels"
-  # kitt.description
-  # => "Pontiac Trans-Am"
-  # kitt.birth_year
-  # => 1982
-  # kitt.salary
-  # => 0
-  # kitt.headshot_url
-  # => "https://goo.gl/lSFYQo"
+  def setup
+    @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+  end
 
   def test_it_exists
-    kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
     assert_instance_of Character, kitt
+  end
+
+  def test_it_has_a_name
+    assert_equal "KITT", kitt.name
+  end
+
+  def test_it_has_an_actor
+    assert_equal "William Daniels", kitt.actor
+  end
+
+  def test_it_has_a_description
+    assert_equal "Pontiac Trans-Am", kitt.description
+  end
+
+  def test_it_has_a_birth_year
+    assert_equal 1982, kitt.birth_year
+  end
+
+  def test_it_has_a_salary
+    assert_equal 0, kitt.salary
+  end
+
+  def test_it_has_a_headshot_url
+    assert_equal "https://goo.gl/lSFYQo", kitt.headshot_url
   end
 
 end
