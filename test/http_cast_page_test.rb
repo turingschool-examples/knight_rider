e4a24ea412d07146880eca18bd7666dca825b2ee
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-02-28 11:36:16 -0700

    holy crap all my http tests pass too

--- test/http_cast_page_test.rb
+++ test/http_cast_page_test.rb
@@ -21,14 +21,28 @@ class HttpCastPageTest < Minitest::Test
   end
 
   def test_it_loads_page_to_server
+    input_file_path = "./io_files/html_output_file_1.html"
+    start_up(9291, input_file_path)
     threads = []
     threads << Thread.new{server.start}
     threads << Thread.new do
       $close = true
-      assert_equal predicted_output, conn.get(path).body
+      assert_equal output_file_1, conn.get("/").body.split("\n").map {|item| item.strip}
     end
     threads.each {|thread| thread.join}
-end
+  end
+
+  def test_it_loads_all_three_characters
+    input_file_path = "./io_files/html_output_file.html"
+    start_up(9293, input_file_path)
+    threads = []
+    threads << Thread.new{server.start}
+    threads << Thread.new do
+      $close = true
+      assert_equal output_file, conn.get("/").body.split("\n").map {|item| item.strip}
+    end
+    threads.each {|thread| thread.join}
+  end
 
   def output_file_1
     ["<html>",
@@ -42,6 +56,39 @@ end
     "<li>35 years old</li>",
     "</ul>",
     "<img src=\"https://goo.gl/lSFYQo\" width=200>",
+    "",
+    "</body>",
+    "</html>"]
+  end
+
+  def output_file
+    ["<html>",
+    "<body>",
+    "<h1>Knight Rider Cast</h1>",
+    "",
+    "<h2>KITT</h2>",
+    "<ul>",
+    "<li>Played by William Daniels</li>",
+    "<li>Pontiac Trans-Am</li>",
+    "<li>35 years old</li>",
+    "</ul>",
+    "<img src=\"https://goo.gl/lSFYQo\" width=200>",
+    "",
+    "<h2>Michael Knight</h2>",
+    "<ul>",
+    "<li>Played by David Hasselhoff</li>",
+    "<li>A special type of hero</li>",
+    "<li>65 years old</li>",
+    "</ul>",
+    "<img src=\"https://goo.gl/Qk0pwL\" width=200>",
+    "",
+    "<h2>Devon Miles</h2>",
+    "<ul>",
+    "<li>Played by Edward Mulhare</li>",
+    "<li>FLAG spokesman and mission coordinator</li>",
+    "<li>94 years old</li>",
+    "</ul>",
+    "<img src=\"https://goo.gl/JsvAuV\" width=200>",
     "",
     "</body>",
     "</html>"]
