667ffbfde46d1d9592ba6db41bdb2c33276fb75d
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-02-28 10:27:57 -0700

    age method works

--- lib/character.rb
+++ lib/character.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Character
-  attr_reader :name, :actor, :description, :birth_year, :salary, :headshot_url
+  attr_reader :name, :actor, :description, :birth_year,
+  :salary, :headshot_url, :age
   def initialize(name, actor, description, birth_year, salary, headshot_url)
     @name = name
     @actor = actor
@@ -9,6 +10,7 @@ class Character
     @birth_year = birth_year
     @salary = salary.delete(",").to_i
     @headshot_url = headshot_url
+    @age = DateTime.now.year.to_i - birth_year
   end
 
 end
