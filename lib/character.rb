f9a6e7b3d6da46a87f005208c184028d3ba01f9b
Sam Landfried <samlandfried@gmail.com>
2017-02-28 09:39:41 -0700

    Pass Character attributes test

--- lib/character.rb
+++ lib/character.rb
@@ -1,4 +1,15 @@
 class Character
+
+  attr_reader :name, :actor, :description, :birth_year, :salary, :headshot_url
+
   def initialize(character_name, actor_name, description, birth_year, salary_string, headshot_url)
+    @name = character_name
+    @actor = actor_name
+    @description = description
+    @birth_year = birth_year
+    @salary = salary.to_i
+    @headshot_url = headshot_url
   end
+
+
 end
