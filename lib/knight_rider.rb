985a0b359b79a2467bdb41acacb69dce2b75efea
NessEFC <NessEFC@gmail.com>
2017-02-28 10:15:25 -0700

    all tests passing for knight rider

--- lib/knight_rider.rb
+++ lib/knight_rider.rb
@@ -1,7 +1,12 @@
 require 'pry'
 
 class KnightRider
+  attr_reader :characters, :creator, :network
 
-
+  def initialize(characters, creator, network)
+    @characters = characters
+    @creator = creator
+    @network = network
+  end
 
 end
--- test/knight_rider_test.rb
+++ test/knight_rider_test.rb
@@ -5,9 +5,28 @@ require './lib/knight_rider'
 require './lib/character'
 
 class KnightRiderTest < Minitest::Test
+  attr_reader :kitt, :michael_knight, :knight_rider
+
+  def setup
+    @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    @knight_rider = KnightRider.new([kitt, michael_knight], "Glen Larson", "NBC")
+  end
 
   def test_it_exists
+    assert_instance_of KnightRider, knight_rider
+  end
 
+  def test_it_has_characters
+    assert_equal [kitt, michael_knight], knight_rider.characters
+  end
+
+  def test_it_has_a_creator
+    assert_equal "Glen Larson", knight_rider.creator
+  end
+
+  def test_it_has_a_network
+    assert_equal "NBC", knight_rider.network
   end
 
 end
