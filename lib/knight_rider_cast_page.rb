21553edf803aeb83432b4eb5fe725a7057815155
ryt11 <rileyt_2000@hotmail.com>
2017-02-28 10:54:14 -0700

    add start of knight_rider_cast_page

--- lib/knight_rider_cast_page.rb
+++ lib/knight_rider_cast_page.rb
@@ -1,14 +1,21 @@
-template_letter = File.read("form_letter.erb") #read contents of our form_letter.erb
-erb_template = ERB.new template_letter #create erb_template which is a new instance of ERB that is passeed the read contents of template_letter
+require './lib/knight_rider'
+require './lib/character'
+require 'erb'
 
 class KnightRiderCastPage
-  attr_reader :letter
-  
-  def initialize(letter)
+  attr_reader :letter, :knight_rider
+
+  def initialize(letter, knight_rider)
     @letter = File.read(letter)
+    @knight_rider = knight_rider
+    binding.pry
   end
 
   def create_erb_template
     ERB.new(letter)
+  end
+
+  def make_page
+    create_erb_template.result
   end
 end
---
+++ test/knight_rider_cast_page_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/knight_rider'
+require './lib/character'
+require './lib/knight_rider_cast_page'
+
+class TestKnightRiderCastPage < Minitest::Test
+  attr_reader :kr, :michael_knight, :kitt, :devon_miles, :actors, :krcp
+  def setup
+    @michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
+    @kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    @devon_miles = Character.new("Devon Miles", "Edward Mulhare", "FLAG spokesman and mission coordinator", 1923, "1,000,000", "https://goo.gl/JsvAuV")
+    @actors = [michael_knight, kitt, devon_miles]
+    @kr = KnightRider.new(actors, "Glen Larson", "NBC")
+    @krcp = KnightRiderCastPage.new("./output/knight_rider_page.erb", kr)
+  end
+
+  def test_that_knight_rider_cast_page_exists
+    assert_equal KnightRiderCastPage, krcp.class
+  end
+
+  def test_that_it_has_knight_rider
+    assert_equal KnightRider, krcp.knight_rider.class
+  end
+
+  def test_that_it_has_erb_file
+    assert_equal ERB, krcp.create_erb_template.class
+
+  end
+end
