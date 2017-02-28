dbdb2414ef75a3d8baea38813b22d4a4a7aba447
somedayrainbows <ebassity@gmail.com>
2017-02-28 11:40:51 -0700

    set up erb and runner

--- lib/html_page.rb
+++ lib/html_page.rb
@@ -1,5 +1,11 @@
 require 'pry'
 require 'erb'
+require './lib/knight_rider'
 
-knight_rider_cast = File.open "html_page.erb"
-erb_template = ERB.new knight_rider_cast.erb
+
+simple_template = knight_rider_cast.erb
+
+renderer = ERB.new(simple_template)
+puts output = renderer.result()
+
+# erb_template = ERB.new knight_rider_cast.erb
