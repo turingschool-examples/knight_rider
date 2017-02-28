88043cb17f9796bc383acd4a12868d8f1924ff66
Jack Pincus <jwpincus@gmail.com>
2017-02-28 11:15:56 -0700

    even more awesome html

--- lib/html.rb
+++ lib/html.rb
@@ -7,8 +7,10 @@ class HTML
   def make_html
     fileHtml = File.new("./html/knight_rider.html", "w+")
     fileHtml.puts "<HTML><BODY BGCOLOR='green'>"
+    fileHtml.puts '<style type="text/css">body, a:hover {cursor: url(http://cur.cursors-4u.net/cursors/cur-3/cur204.ani), url(http://cur.cursors-4u.net/cursors/cur-3/cur204.png), progress !important;}</style><a href="http://www.cursors-4u.com/cursor/2009/04/23/flaming-arrow.html" target="_blank" title="Flaming Arrow"><img src="http://cur.cursors-4u.net/cursor.png" border="0" alt="Flaming Arrow" style="position:absolute; top: 0px; right: 0px;" /></a>'
     fileHtml.puts "<CENTER><h1>Knight Rider Cast</h1>"
     fileHtml.puts "<iframe width=\"0\" height=\"0\" src=\"https://www.youtube.com/embed/Mo8Qls0HnWo?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>"
+    fileHtml.puts '<img src="http://www.textfiles.com/underconstruction/MoMotorCity4109construct.gif" width=\"70%\">'
     character_format(fileHtml)
     fileHtml.puts "</CENTER></BODY></HTML>"
     fileHtml.close()
