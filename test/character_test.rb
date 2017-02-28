ea7cd92afd94b26ac5518ef52cb564f105435637
bschwartz10 <bschwartz92@gmail.com>
2017-02-28 09:39:48 -0700

    add michael_knight character

--- test/character_test.rb
+++ test/character_test.rb
@@ -7,44 +7,58 @@ class CharacterTest < Minitest::Test
 
   def test_it_exists
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
 
     assert_instance_of Character, kitt
+    assert_instance_of Character, michael_knight
   end
 
   def test_character_has_a_name
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
 
     assert_equal "KITT", kitt.name
+    assert_equal "Michael Knight", michael_knight.name
   end
 
   def test_character_has_an_actor
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
 
     assert_equal "William Daniels", kitt.actor
+    assert_equal "David Hasselhoff", michael_knight.actor
   end
 
   def test_character_has_a_description
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
 
     assert_equal "Pontiac Trans-Am", kitt.description
+    assert_equal "A special type of hero", michael_knight.description
   end
 
   def test_character_has_a_birth_year
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
 
     assert_equal 1982, kitt.birth_year
+    assert_equal 1952, michael_knight.birth_year
   end
 
   def test_character_has_a_salary
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
 
     assert_equal "0.00", kitt.salary
+    assert_equal "1,000,000,000.00", michael_knight.salary
   end
 
   def test_character_has_a_headshot_url
     kitt = Character.new("KITT", "William Daniels", "Pontiac Trans-Am", 1982, "0.00", "https://goo.gl/lSFYQo")
+    michael_knight = Character.new("Michael Knight", "David Hasselhoff", "A special type of hero", 1952, "1,000,000,000.00", "https://goo.gl/Qk0pwL")
 
     assert_equal "https://goo.gl/lSFYQo", kitt.headshot_url
+    assert_equal "https://goo.gl/Qk0pwL", michael_knight.headshot_url
   end
 
 end
